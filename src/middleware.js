092e72d3d4f9c4e4c3d5c84ced66a90977e6dd2f
rajan <Prajapatirajan776@gmail.com>
2025-06-06 18:19:11 +0530

    Initial commit from Create Next App

---
+++ src/middleware.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { JWT_SECRET } from "./app/(auth)/api/signIn/route";
+import { jwtVerify } from "jose";
+
+export async function middleware(req) {
+  try {
+    const token = req.cookies.get(process.env.COOKIE_NAME)?.value;
+    const isSignInPage = req.nextUrl.pathname === "/signIn";
+    const Homepage = req.nextUrl.pathname === "/";
+
+    if (!token) {
+      if (isSignInPage) {
+        return NextResponse.next();
+      }
+      return NextResponse.redirect(new URL("/signIn", req.url));
+    }
+    const decoded = await jwtVerify(
+      token,
+      new TextEncoder().encode(JWT_SECRET)
+    );
+    if (!decoded) {
+      return NextResponse.redirect(new URL("/signIn", req.url));
+    }
+    if (Homepage) {
+      return NextResponse.redirect(new URL("/instagram", req.url));
+    }
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Token verification error:", error);
+    return NextResponse.json(
+      { error: "Invalid or expired token" },
+      { status: 401 }
+    );
+  }
+}
+
+export const config = {
+  matcher: ["/", "/api/:path*"],
+};
